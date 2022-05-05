0b2f56e33c2a2279e2700a542b7c1df17aacabbb
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-05 16:49:44 +0200

    aggiunta css snack-3

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,7 +1,7 @@
 // Crea due tag div con due id diversi:
 // un div avrà il testo colorato di rosso mentre l’altro di verde
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari
-const numbers = [12,34,57,86,39,76,36,77,81, 124];
+const numbers = [12,34,57,86,39,76,36,77,81,124];
 let oddNumbers = [];
 let evenNumbers = [];
 console.log(numbers);
