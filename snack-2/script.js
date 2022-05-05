f135818061e1c59e20eeae2879229b4fbe189646
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-05 15:45:54 +0200

    fix snack-2

--- snack-2/script.js
+++ snack-2/script.js
@@ -2,38 +2,11 @@
 let casualNumber = Number(prompt("Inserisci un numero"));
 console.log(casualNumber);
 // Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100
-const arrayGen = [];
 for (let i = 0 ; i < casualNumber ; i++) {
+    const arrayGen = [];
     for (let i = 0 ; i < 10 ; i++) {
         arrayGen.push(Math.floor(Math.random() * 100) + 1);
     }
+    console.log(arrayGen);
 };
-console.log(arrayGen);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Ogni volta che ne crei uno, stampalo a schermo
