4d6f7fa603117bfa9e045351c6364b46e656c55a
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-05 15:24:49 +0200

    prova js

--- snack-2/script.js
+++ snack-2/script.js
@@ -2,11 +2,13 @@
 const casualNumber = Number(prompt("Inserisci un numero"));
 console.log(casualNumber);
 // Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100
-const arrayGen = [];
-for (let i = 0 ; i < 10 ; i++) {
-    arrayGen.push(Math.floor(Math.random() * 100) + 1);
-};
-console.log(arrayGen);
+while (casualNumber) {
+    const arrayGen = [];
+    for (let i = 0 ; i < 10 ; i++) {
+        arrayGen.push(Math.floor(Math.random() * 100) + 1);
+    };
+    console.log(arrayGen);
+}
 
 
 
