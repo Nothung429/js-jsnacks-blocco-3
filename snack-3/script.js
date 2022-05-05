3ddae0e122652ce43d5797cb210ea1f6e47b59e5
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-05 15:51:10 +0200

    inizio snack-3

---
+++ snack-3/script.js
@@ -0,0 +1,4 @@
+// Crea due tag div con due id diversi:
+// un div avrà il testo colorato di rosso mentre l’altro di verde
+// Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari
+const numbers = [12,34,57,86,39,76,36,77,81];
