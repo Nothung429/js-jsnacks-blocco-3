b654c3732c906ec6f9db5f819ddbe253a3f13b67
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-05 13:03:50 +0200

    correzione snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,9 +1,11 @@
 // Crea un array vuoto e chiedi all’utente un numero da inserire nell’array
 // Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50
-const arrayUser = [];
+const userNumbers = [];
+let somma = 0;
 do {
-    const numberRequest = Number(prompt("Inserisci un numero"));
-    console.log(numberRequest);
-    arrayUser.push(numberRequest);
-} while (arrayUser < 50);
-console.log(arrayUser);
+    let numberRequest = Number(prompt("Inserisci un numero"))
+    somma = somma + numberRequest;
+    userNumbers.push(numberRequest)
+    console.log(userNumbers);
+} while (somma < 50);
+console.log(somma);
